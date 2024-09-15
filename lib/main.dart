b9977907e2f33030dcac722b1670ab2dfd97fe7f
mhr <mhnrahmani@gmail.com>
2024-09-15 17:07:16 +0200

    add external profile links

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portfolio/firebase_options.dart';
 import 'package:portfolio/image_from_firebase.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,7 +14,28 @@ void main() async {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  final String profilePhotoPath = 'img/profile.jpg';
+  final String pathProfile = 'img/profile.jpg';
+
+  // Define the URLs
+  final String linkedinUrl = 'https://www.linkedin.com/in/mhnrahmani/';
+  final String scholarUrl =
+      'https://scholar.google.com/citations?user=Avb2u10AAAAJ';
+  final String universityUrl =
+      'https://www.uantwerpen.be/en/staff/mohammad-rahmani/';
+  final String orcidUrl = 'https://orcid.org/0000-0002-0241-8270';
+  final String researchgateUrl =
+      'https://www.researchgate.net/profile/Mohammad-Hasan-Rahmani-2';
+  final String githubUrl = 'https://github.com/mhnrahmani/';
+
+  // Method to launch URLs
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,23 +44,47 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to my homepage!'),
-          // actions: const [ImageFromFirebase(filePath: 'img/profile.jpg')],
+          title: const Text(
+              'I am adding a timeline of my projects and more information about me. Stay tuned!'),
+          actions: [
+            IconButton(
+              icon: const Icon(FontAwesomeIcons.orcid),
+              onPressed: () => _launchURL(orcidUrl),
+              tooltip: 'ORCID Profile',
+            ),
+            IconButton(
+              icon: const Icon(FontAwesomeIcons.googleScholar),
+              onPressed: () => _launchURL(scholarUrl),
+              tooltip: 'Google Scholar Profile',
+            ),
+            IconButton(
+              icon: const Icon(FontAwesomeIcons.researchgate),
+              onPressed: () => _launchURL(researchgateUrl),
+              tooltip: 'ResearchGate Profile',
+            ),
+            IconButton(
+              icon: const Icon(FontAwesomeIcons.github),
+              onPressed: () => _launchURL(githubUrl),
+              tooltip: 'GitHub Profile',
+            ),
+            IconButton(
+              icon: const Icon(FontAwesomeIcons.linkedin),
+              onPressed: () => _launchURL(linkedinUrl),
+              tooltip: 'LinkedIn Profile',
+            ),
+            const SizedBox(width: 10), // Adds some space before the edge
+          ],
           centerTitle: true,
         ),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              const Text(
-                'This site is still under construction. I am adding a timeline of my projects and more information about me. Stay tuned!',
-                style: TextStyle(fontSize: 24),
-              ),
               const SizedBox(height: 40),
               Center(
                 child: FractionallySizedBox(
                   widthFactor: 0.4,
-                  child: ClipOval(
-                      child: ImageFromFirebase(filePath: profilePhotoPath)),
+                  child:
+                      ClipOval(child: ImageFromFirebase(filePath: pathProfile)),
                 ),
               ),
             ],
