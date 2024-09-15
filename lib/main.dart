07a6e8a0f749dcd530c82545df5d2760bc4e4ccf
mhr <mhnrahmani@gmail.com>
2024-09-15 16:34:46 +0200

    improve structure

--- lib/main.dart
+++ lib/main.dart
@@ -12,18 +12,36 @@ void main() async {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  final String profilePhotoPath = 'img/profile.jpg';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'M.H. Rahmani: Portfolio',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to my homepage!'),
-          actions: const [ImageFromFirebase(filePath: 'img/profile.jpg')],
+          // actions: const [ImageFromFirebase(filePath: 'img/profile.jpg')],
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text('Hello World!'),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Text(
+                'This site is still under construction. I am adding a timeline of my projects and more information about me. Stay tuned!',
+                style: TextStyle(fontSize: 24),
+              ),
+              const SizedBox(height: 40),
+              Center(
+                child: FractionallySizedBox(
+                  widthFactor: 0.4,
+                  child: ClipOval(
+                      child: ImageFromFirebase(filePath: profilePhotoPath)),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
